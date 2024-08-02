b2068dd8bbc69fa76dc7a5c304366a5a6a9bbe83
edresvyannikov <dresvyannikov7@gmail.com>
2024-08-02 19:32:52 +0400

    NumberOfIslands dfs

--- top150/src/main/java/graph/NumberOfIslands.java
+++ top150/src/main/java/graph/NumberOfIslands.java
@@ -5,13 +5,12 @@ import java.util.*;
 public class NumberOfIslands {
     char[][] board;
 
-    final static int[][] directions = new int[][] { {1, 0}, {-1, 0}, {0, 1}, {0, -1} };
-
     public static void main(String[] args) {
         System.out.println(new NumberOfIslands().numIslands(new char[][]{
-                {'1','0','1','1','1'},
-                {'1','0','1','0','1'},
-                {'1','1','1','0','1'}
+                {'1','1','1','1','0'},
+                {'1','1','0','1','0'},
+                {'1','1','0','0','0'},
+                {'0','0','0','0','0'}
         }));
     }
 
@@ -25,7 +24,7 @@ public class NumberOfIslands {
                 if (board[i][j] == '0' || visited.contains(Map.entry(i, j))) {
                     continue;
                 }
-                bfs(i, j, visited);
+                dfs(i, j, visited);
                 counter++;
             }
         }
@@ -33,32 +32,18 @@ public class NumberOfIslands {
         return counter;
     }
 
-    private void bfs(int i, int j,  Set<Map.Entry<Integer, Integer>> visited) {
+    private void dfs(int i, int j,  Set<Map.Entry<Integer, Integer>> visited) {
 
-        Queue<Map.Entry<Integer, Integer>> current = new ArrayDeque<>();
-        Queue<Map.Entry<Integer, Integer>> next = new ArrayDeque<>();
-
-        current.add(Map.entry(i, j));
-
-        while (!current.isEmpty()) {
-            Map.Entry<Integer, Integer> pop = current.poll();
-            int row = pop.getKey();
-            int col = pop.getValue();
-            visited.add(Map.entry(row, col));
-
-            for (int k = 0; k < directions.length; k++) {
-                int r = row + directions[k][0];
-                int c = col + directions[k][1];
-                if (r > -1 && r < board.length && c > -1 && c < board[0].length && board[r][c] == '1' && !visited.contains(Map.entry(r, c))) {
-                    next.add(Map.entry(r, c));
-                }
-            }
-
-            if (current.isEmpty()) {
-                while (!next.isEmpty()) {
-                    current.add(next.poll());
-                }
-            }
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] == '0' || visited.contains(Map.entry(i, j))) {
+            return;
         }
+
+        board[i][j] = '0';
+
+        dfs(i, j+1, visited);
+        dfs(i, j-1, visited);
+        dfs(i+1, j, visited);
+        dfs(i-1, j, visited);
+
     }
 }
